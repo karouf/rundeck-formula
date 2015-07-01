4730feb1c48d9d57a2443e0aef4231a3b90a0a68
Renaud Martinet <karouf@gmail.com>
2015-07-01 17:30:29 +0200

    Check project.properties defaults

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -26,3 +26,13 @@ describe file('/etc/rundeck/rundeck-config.properties') do
   its(:content) { should match /^grails.serverURL = http:\/\/rundeck.local:4440$/ }
   its(:content) { should match /^rundeck.projectsStorageType = filesystem$/ }
 end
+
+describe file('/etc/rundeck/project.properties') do
+  it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by 'rundeck' }
+  it { should be_grouped_into 'rundeck' }
+  its(:content) { should match /^project.dir = \/var\/rundeck\/projects\/\$\{project\.name\}$/ }
+  its(:content) { should match /^project.etc.dir = \/var\/rundeck\/projects\/\$\{project\.name\}\/etc$/ }
+  its(:content) { should match /^project.resources.file = \/var\/rundeck\/projects\/\$\{project\.name\}\/etc\/resources\.xml$/ }
+end
