b32da756bee70d5fd03b893db69ae2f1f1522de1
Renaud Martinet <karouf@gmail.com>
2015-07-09 16:53:19 +0200

    Remove unrelated specs from the config suite

--- test/integration/config/serverspec/auth_spec.rb
+++ test/integration/config/serverspec/auth_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe file('/etc/rundeck/realm.properties') do
-  it { should be_file }
-  it { should be_mode 644 }
-  it { should be_owned_by 'rundeck' }
-  it { should be_grouped_into 'rundeck' }
   its(:content) { should match /^admin:admin,user,admin$/ }
   its(:content) { should match /^user:passwd,user$/ }
 end
