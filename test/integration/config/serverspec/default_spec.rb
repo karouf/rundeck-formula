d28965f45a327413a6780b5c1d0561766936d18d
Renaud Martinet <karouf@gmail.com>
2015-07-09 17:15:15 +0200

    Configure listening port through pillar

--- test/integration/config/serverspec/default_spec.rb
+++ test/integration/config/serverspec/default_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe file('/etc/rundeck/rundeck-config.properties') do
-  its(:content) { should match /^grails.serverURL = http:\/\/rundeck.local:4440$/ }
+  its(:content) { should match /^grails.serverURL = http:\/\/rundeck.local:80$/ }
 end
